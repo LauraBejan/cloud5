fd7f467dddf117daaf2c8aa1838c01d7f314fe85
LauraBejan <bejan_laura@yahoo.com>
2019-05-16 20:56:57 +0000

    Added mail function

---
+++ sendMail.py
@@ -0,0 +1,24 @@
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+def sendMail(mail):
+	f = open("mail_key.txt", "r")
+	key=f.read()
+
+	message = Mail(from_email = 'lepowobo@directmail.top',
+			to_emails=mail,
+			subject='You have a added a new song to the playlist,
+			plain_text_content='easy to use',
+			html_content='''This is the song you added. 
+							Title:
+							Artist:
+							Lyrics:
+							Look for recomendations''')
+	try:
+		sg=SendGridAPIClient(key)
+		response=sg.send(message)
+		print(key)
+
+	except Exception as e:
+		print(e)
