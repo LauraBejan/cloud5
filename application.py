18cd15e40cf0c19d39ffb32f1bc004901dd1b244
AdrianHamod <adrianhamod@yahoo.com>
2019-05-16 21:42:22 +0000

    added form for question

--- application.py
+++ application.py
@@ -1,14 +1,32 @@
 from dbconn import *
 from yt_api import *
-from flask import Flask
+from flask import Flask,render_template, flash, request
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+import json
 app = Flask(__name__)
-
+class ReusableForm(Form):
+    name = TextField('Name:', validators=[validators.required()])
 @app.route("/")
 def hello():
+    
     # message_to_display = ""
     # row = customQuery("SELECT * FROM PLAYLISTS")
     # for word in row:
     #     message_to_display += str(word)
     # return message_to_display
     # return("gets here?")
-    return str(getRandomAnswer('kim kardashian'))
+    
+    return render_template("hello.html")
+    # return str(getRandomAnswer('kim kardashian'))
+
+@app.route("/result",methods=['POST','GET'])
+def result():
+    if request.method == 'POST':
+        result = request.form["Question"]
+        if len(result) < 3:
+            return str(getRandomAnswer(result))
+        else:
+            search = " ".join(result.split(" ")[-2:])
+            return str(getRandomAnswer(search))
+    #   return str(result)
+    #   return render_template("result.html",result = result)
