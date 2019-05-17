c3623c43d455e60b83d90f8648354e11a8010a3e
LauraBejan <bejan_laura@yahoo.com>
2019-05-17 01:00:11 +0000

    a

--- application.py
+++ application.py
@@ -5,22 +5,22 @@ from sendMail import sendMail
 from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 import json
 from sendMail import sendMail
+from translator import translateText
+
 
 from flask import Flask
+
 app = Flask(__name__)
 class ReusableForm(Form):
     name = TextField('Name:', validators=[validators.required()])
 @app.route("/")
 def hello():
-    
     # message_to_display = ""
     # row = customQuery("SELECT * FROM PLAYLISTS")
     # for word in row:
     #     message_to_display += str(word)
     # return message_to_display
     # return("gets here?")
-    receiver="lepowobo@directmail.top"
-    sendMail(receiver)
     return render_template("hello.html")
     # return str(getRandomAnswer('kim kardashian'))
 
@@ -36,3 +36,23 @@ def result():
     #   return str(result)
     #   return render_template("result.html",result = result)
     return str(getRandomAnswer('kim kardashian'))
+
+
+@app.route("/mail")
+def mail():
+    return render_template("mail.html")
+
+
+
+@app.route("/preview",methods=['POST','GET'])
+def preview():
+    if request.method == 'POST':
+            receiver = request.form["receiver"]
+            title = request.form["title"]
+            message = request.form["message"]
+            sendMail(receiver,title,message)
+    return "Mail sent"
+
+@app.route("/translate")
+def translate():
+    return translateText("salut","english")
--- sendMail.py
+++ sendMail.py
@@ -2,19 +2,15 @@ import os
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
-def sendMail(mail):
+def sendMail(to,title,message):
 	f = open("mail_key.txt", "r")
 	key=f.read()
 
-	message = Mail(from_email = 'lepowobo@directmail.top',
-			to_emails=mail,
-			subject='You have a added a new song to the playlist',
+	message = Mail(from_email = "lepowobo@directmail.top",
+			to_emails=to,
+			subject=title,
 			plain_text_content='easy to use',
-			html_content='''This is the song you added. 
-							Title:
-							Artist:
-							Lyrics:
-							Look for recomendations''')
+			html_content=message)
 	try:
 		sg=SendGridAPIClient(key)
 		response=sg.send(message)
